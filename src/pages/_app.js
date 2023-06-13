2ae073f9677c0b96e70cbe595ed3b89403b8b056
Guri-X <singhhh890.gs@gmail.com>
2023-06-13 16:13:07 +0530

    Added google analytics tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,6 +11,7 @@ import "nprogress/nprogress.css";
 import "swiper/css";
 import "swiper/css/navigation";
 import "../styles/globals.scss";
+import Script from 'next/script';
 
 NProgress.configure({ showSpinner: true });
 Router.events.on("routeChangeStart", () => NProgress.start());
@@ -18,40 +19,55 @@ Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  const queryClientRef = useRef();
+	const queryClientRef = useRef();
 
-  if (!queryClientRef.current) {
-    queryClientRef.current = new QueryClient();
-  }
+	if (!queryClientRef.current) {
+		queryClientRef.current = new QueryClient();
+	}
 
-  return (
-    <QueryClientProvider client={queryClientRef.current}>
-      <AppProvider>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1 maximum-scale=1"
-          />
-          <meta name="description" content="description" />
-          <meta name="robots" content="noindex, nofollow" />
-          <meta name="keywords" content="React Template, Portfolio Template" />
-          <meta name="author" content="Nuclear Themes" />
-          <meta name="theme-color" content="#72E2AE" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <link rel="manifest" href="./manifest.json" />
-          <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
-          <link rel="apple-touch-icon" href="./icon-192x192.png" />
-          <title>Bieber - React Personal Portfolio Template</title>
-        </Head>
-        <ParallaxProvider>
-          <Hydrate state={pageProps.dehydratedState}>
-            <Component {...pageProps} />
-          </Hydrate>
-        </ParallaxProvider>
-      </AppProvider>
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClientRef.current}>
+			<AppProvider>
+				<Head>
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1 maximum-scale=1"
+					/>
+					<meta name="description" content="description" />
+					<meta name="robots" content="noindex, nofollow" />
+					<meta name="keywords" content="React Template, Portfolio Template" />
+					<meta name="author" content="Nuclear Themes" />
+					<meta name="theme-color" content="#72E2AE" />
+					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+					<link rel="manifest" href="./manifest.json" />
+					<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+					<link rel="apple-touch-icon" href="./icon-192x192.png" />
+					<title>Bieber - React Personal Portfolio Template</title>
+				</Head>
+
+				{/* <!-- Google tag (gtag.js) --> */}
+				<Script src="https://www.googletagmanager.com/gtag/js?id=G-SMSCM7HYDP" />
+				<Script>
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag() {
+							dataLayer.push(arguments);
+						}
+						gtag("js", new Date());
+
+						gtag("config", "G-SMSCM7HYDP");
+					`}
+				</Script>
+
+				<ParallaxProvider>
+					<Hydrate state={pageProps.dehydratedState}>
+						<Component {...pageProps} />
+					</Hydrate>
+				</ParallaxProvider>
+			</AppProvider>
+			{/* <ReactQueryDevtools initialIsOpen={false} /> */}
+		</QueryClientProvider>
+	);
 }
 
 export default MyApp;
